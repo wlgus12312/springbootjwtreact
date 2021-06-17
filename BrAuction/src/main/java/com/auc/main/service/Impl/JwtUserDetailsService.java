36153dd1e268c3cdf4bc0b215d4acfc50026bb15
kjh <kjh@DESKTOP-I0BBURQ>
2021-06-17 11:19:07 +0900

    1

--- BrAuction/src/main/java/com/auc/main/service/Impl/JwtUserDetailsService.java
+++ BrAuction/src/main/java/com/auc/main/service/Impl/JwtUserDetailsService.java
@@ -13,13 +13,15 @@ public class JwtUserDetailsService implements UserDetailsService{
 	
 	   @Override
 	    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		    
+		   //DB 사용자 id, password 조회해서 비교
 	        if ("user_id".equals(username)) {
 	            return new User("user_id", "$2a$10$m/enYHaLsCwH2dKMUAtQp.ksGOA6lq7Fd2pnMb4L.yT4GyeAPRPyS",
 	                new ArrayList<>());
 	        } else {
 	            throw new UsernameNotFoundException("User not found with username: " + username);
 	        }
-	    }
+	    } 
 	
 	
 }
